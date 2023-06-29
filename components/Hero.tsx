139b01a8bdb4620c9e5e837f3353c6073cae04bc
valensixx <valensixx@gmail.com>
2023-06-29 18:24:55 +0300

    add new public assets

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -25,6 +25,11 @@ const Hero = () => {
                     handleClick={handleScroll}
                 />
             </div>
+            <div className="hero__image-container">
+                <div className="hero_image">
+                    <Image src="" />
+                </div>
+            </div>
         </div>
     )
 }
