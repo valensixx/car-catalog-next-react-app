2f29ad06b06bf385ea176eb8b0fb0cd6fdd72cd0
valensixx <valensixx@gmail.com>
2023-06-29 18:46:39 +0300

    fixed error - to large car image

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -13,11 +13,11 @@ const Hero = () => {
         <div className="hero">
             <div className="flex-1 pt-36 padding-x">
                 <h1 className="hero__title">
-                    Find, book, or rent a car - quickly and easily!
+                    Find, book, rent a car—quick and super easy!
                 </h1>
                 <p className="hero__subtitle">
-                    Streamline your car rental experiance
-                    with our effortless booking process.
+                    Streamline your car rental experience with our effortless booking
+                    process.
                 </p>
                 <CustomButton
                     title="Explore Cars"
@@ -26,10 +26,10 @@ const Hero = () => {
                 />
             </div>
             <div className="hero__image-container">
-                <div className="hero_image">
+                <div className="hero__image">
                     <Image src="/hero.png" alt="hero" fill className="object-contain" />
-                    <div className="hero__image-overlay"/>
                 </div>
+                <div className="hero__image-overlay" />
             </div>
         </div>
     )
