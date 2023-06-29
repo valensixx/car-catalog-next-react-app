f098e7723d135ed24b5912b45ecb68e06358e030
valensixx <valensixx@gmail.com>
2023-06-29 14:38:23 +0300

    working on Hero.tsx

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -5,7 +5,14 @@ const Hero = () => {
   return (
     <div className="hero">
         <div className="flex-1 pt-36 padding-x">
-            
+            <h1 className="hero__title">
+                Find, book, or rent a car - quickly and easily!
+            </h1>
+            <p className="hero__subtitle">
+                Streamline your car rental experiance
+                with our effortless booking process.
+            </p>
+            <CustomButton />
         </div>
     </div>
   )
