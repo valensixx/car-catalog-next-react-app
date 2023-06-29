a0b72442fd22cca267dcc6d6b45afafa1e95102d
valensixx <valensixx@gmail.com>
2023-06-29 18:02:59 +0300

    working on CustomButton, Hero, connectiong props

--- components/CustomButton.tsx
+++ components/CustomButton.tsx
@@ -1,8 +1,21 @@
-import React from 'react';
+"use client";
 
-const CustomButton = () => {
+import React from 'react';
+import Image from 'next/image';
+import { CustomButtonProps } from '@/types';
+
+const CustomButton = ({title, containerStyles,handleClick}: CustomButtonProps ) => {
   return (
-    <div>CustomButton</div>
+    <button 
+        disabled={false}
+        type={"button"}
+        className={`custom-btm`}
+        onClick={()=> {}}
+    >
+        <span className={`flex-1`}>
+            title
+        </span>
+    </button>
   )
 }
 
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,7 +1,14 @@
+"use client";
+
 import React from 'react';
 import Image from 'next/image';
+import CustomButton from './CustomButton';
 
 const Hero = () => {
+    const handleScroll = () => {
+
+    }
+
   return (
     <div className="hero">
         <div className="flex-1 pt-36 padding-x">
@@ -12,7 +19,11 @@ const Hero = () => {
                 Streamline your car rental experiance
                 with our effortless booking process.
             </p>
-            <CustomButton />
+            <CustomButton 
+                title = "Explore Cars" 
+                containerStyles = "bg-primary-blue text-white, rounded-full mt-10"
+                handleClick = {handleScroll}
+            />
         </div>
     </div>
   )
