5f9e2e514c5827803fe632cc3ea1013ceabc5e26
valensixx <valensixx@gmail.com>
2023-06-30 14:36:39 +0300

    Footer done

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -27,6 +27,17 @@ const Footer = () => {
           ))}
         </div>
       </div>
+      <div className="flex justify-between items-center flex-wrap mt-10 border-t border-grey-100 sm:px-16 px-6 py-10">
+        <p>@2023 Car Catalog. All Rights Reserved</p>
+        <div className="footer__copyrights-link">
+          <Link href="/" className='text-grey-500'>
+            Privacy Policy
+          </Link>
+          <Link href="/" className='text-grey-500'>
+            Terms of Use
+          </Link>
+        </div>
+      </div>
     </footer>
   );
 };
