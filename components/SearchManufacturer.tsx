bfe84faa8323b008f12a2dded0cf1ee925b1f812
valensixx <valensixx@gmail.com>
2023-06-30 17:19:33 +0300

    changes

--- components/SearchManufacturer.tsx
+++ components/SearchManufacturer.tsx
@@ -1,6 +1,7 @@
-import React from 'react'
+import { SearchManufacturerProps } from '@/types';
+import React from 'react';
 
-const SearchManufacturer = () => {
+const SearchManufacturer = ({manufacturer, setManufacturer}: SearchManufacturerProps) => {
   return (
     <div>SearchManufacturer</div>
   )
