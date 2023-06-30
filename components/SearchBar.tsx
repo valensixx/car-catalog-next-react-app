8b3f9851e5291eb88f4f074431331f0806f2dac7
valensixx <valensixx@gmail.com>
2023-06-30 17:11:21 +0300

    create SearchManufacturer.tsx and connectd it

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,12 +1,15 @@
 "use client";
 import React from 'react';
+import SearchManufacturer from './SearchManufacturer';
 
 const SearchBar = () => {
     const handleSearch = () => {}
 
   return (
     <form className="searchbar" onSubmit={handleSearch}>
-
+        <div className="searchbar__item">
+            <SearchManufacturer  />
+        </div>
     </form>
   )
 }
--- components/index.ts
+++ components/index.ts
@@ -4,11 +4,13 @@ import Navbar from "./Navbar";
 import Footer from "./Footer";
 import SearchBar from "./SearchBar";
 import CustomFilter from "./CustomFilter";
+import SearchManufacturer from "./SearchManufacturer";
 export {
     Hero,
     CustomButton,
     Navbar,
     Footer,
     SearchBar,
-    CustomFilter
+    CustomFilter,
+    SearchManufacturer
 }
