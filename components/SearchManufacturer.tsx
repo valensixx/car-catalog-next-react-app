fb0d9ca15ccb3da77a653c7e2a77e85fe55fb480
valensixx <valensixx@gmail.com>
2023-07-02 12:02:52 +0300

    working on input box end transitions

--- components/SearchManufacturer.tsx
+++ components/SearchManufacturer.tsx
@@ -1,11 +1,14 @@
 "use client"
 
+import {useState, Fragment} from 'react';
 import Image from 'next/image';
 import { SearchManufacturerProps } from '@/types';
 import React from 'react';
 import { Combobox, Transition } from '@headlessui/react';
 
 const SearchManufacturer = ({manufacturer, setManufacturer}: SearchManufacturerProps) => {
+  const [query, setQuery] = useState('');
+
   return (
     <div className="search-manufacturer">
       <Combobox>
@@ -19,6 +22,22 @@ const SearchManufacturer = ({manufacturer, setManufacturer}: SearchManufacturerP
               alt = "Car logo"
             />
           </Combobox.Button>
+          <Combobox.Input 
+            className="search-manufacturer__input"
+            placeholder="Volkswagen"
+            displayValue ={(manufacturer: string) => manufacturer}
+            onChange = {(e) => setQuery(e.target.value)} 
+          />
+
+          <Transition 
+            as={Fragment}
+            leave="transition ease-in duration-100"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+            afterLeave={()=> setQuery('')}
+          >
+
+          </Transition>
         </div>
       </Combobox>
     </div>
