ead910b9efd140dcf7975fbd6f61c3d824d068f2
valensixx <valensixx@gmail.com>
2023-07-02 11:42:33 +0300

    working on search manufacturer

--- components/SearchManufacturer.tsx
+++ components/SearchManufacturer.tsx
@@ -1,9 +1,27 @@
+"use client"
+
+import Image from 'next/image';
 import { SearchManufacturerProps } from '@/types';
 import React from 'react';
+import { Combobox, Transition } from '@headlessui/react';
 
 const SearchManufacturer = ({manufacturer, setManufacturer}: SearchManufacturerProps) => {
   return (
-    <div>SearchManufacturer</div>
+    <div className="search-manufacturer">
+      <Combobox>
+        <div className="relative w-full">
+          <Combobox.Button className="absolute top-[14px]">
+            <Image 
+              src="/car-logo.svg"
+              width ={20}
+              height={20}
+              className = "ml-4"
+              alt = "Car logo"
+            />
+          </Combobox.Button>
+        </div>
+      </Combobox>
+    </div>
   )
 }
 
