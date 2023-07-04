dfa097ca80d9e06ca3bf845b4a4761df1880893d
valensixx <valensixx@gmail.com>
2023-07-04 14:42:09 +0300

    component done

--- components/CarDetails.tsx
+++ components/CarDetails.tsx
@@ -40,7 +40,7 @@ const CarDetails = ({ isOpen, closeModal, car }: CarDetailsProps) => {
                 leaveTo="opacity-0 scale-95"
               >
                 <Dialog.Panel className="relative w-full max-w-lg max-h-[90vh] overflow-y-auto
-                transform rounded-2x1 bg-white text-left shadow-xsl transition-all 
+                transform rounded-2xl bg-white text-left p-6 shadow-xl transition-all 
                 flex flex-col gap-5">
                   <button
                     type="button"
@@ -69,6 +69,35 @@ const CarDetails = ({ isOpen, closeModal, car }: CarDetailsProps) => {
                           alt="car-model" fill priority className="object-contain"
                         />
                       </div>
+                      <div className="flex-1 relative w-full h-24 bg-primary-blue-100 rounded-lg">
+                        <Image
+                          src="/hero.png"
+                          alt="car-model" fill priority className="object-contain"
+                        />
+                      </div>
+                      <div className="flex-1 relative w-full h-24 bg-primary-blue-100 rounded-lg">
+                        <Image
+                          src="/hero.png"
+                          alt="car-model" fill priority className="object-contain"
+                        />
+                      </div>
+                    </div>
+                  </div>
+                  <div className="flex-1 flex flex-col gap-2">
+                    <h2 className="font-semibold text-el capitalize">
+                      {car.make} {car.model}
+                    </h2>
+                    <div className="mt-3 flex flex-wrap gap-4">
+                      {Object.entries(car).map(([key, value]) => (
+                        <div className="flex justify-between gap-5 w-full text-right" key={key}>
+                          <h4 className="text-grey capitalize">
+                            {key.split("_").join(" ")}
+                          </h4>
+                          <p className="text-black-100 font-semibold">
+                            {value}
+                          </p>
+                        </div>
+                      ))}
                     </div>
                   </div>
                 </Dialog.Panel>
