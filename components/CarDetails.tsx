4d630cb3149417e290bd7dc986604bde53e38396
valensixx <valensixx@gmail.com>
2023-07-04 12:50:19 +0300

    working on transitioning

--- components/CarDetails.tsx
+++ components/CarDetails.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { CarProps } from '@/types';
 import React from 'react';
 import Image from 'next/image';
@@ -15,8 +17,23 @@ const CarDetails = ({isOpen, closeModal, car}: CarDetailsProps) => {
     <>
       <Transition appear show={isOpen} as={Fragment}>
         <Dialog as="div" className="relative z-10" onClose={closeModal}>
-          
-        </Dialog>
+          <Transition.Child 
+            as={Fragment}
+            enter="ease-out duration-300"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+            leave="ease-in duration-200"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
+            <div className="fixed inset-0 bg-black bg-opacity-25"/>
+          </Transition.Child>
+          <div className="fixed inset-0 overflow-y-auto">
+            <div className="flex min-h-full items-center justify-center p-4 text-center">
+
+            </div>
+          </div>
+          </Dialog>
       </Transition>
     </>
   )
