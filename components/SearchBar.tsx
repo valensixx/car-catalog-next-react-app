6b7fe9f1b6a332cb166d353612ac8cccee70ef08
valensixx <valensixx@gmail.com>
2023-07-05 14:28:20 +0300

    working on SearchBar component

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -3,10 +3,25 @@ import React from 'react';
 import SearchManufacturer from './SearchManufacturer';
 import { useState } from 'react';
 import { manufacturers } from '@/constants';
+import Image from 'next/image';
 
 const SearchBar = () => {
-    const [manufacturer, setManufacturer] = useState('');
+  
+  const SearchButton = ({otherClasses} : { otherClasses: string }) => (
+    <button type="submit" className={`-ml-3 z-10 ${otherClasses}`}>
+      <Image 
+        src={"/magnifying-glass.svg"}
+        alt="magnigying glass"
+        width = {40}
+        height = {40}
+        className = "object-contain"
+      />
+    </button>
+  )
+
+  const [manufacturer, setManufacturer] = useState('');
     const handleSearch = () => {}
+
 
   return (
     <form className="searchbar" onSubmit={handleSearch}>
@@ -15,6 +30,7 @@ const SearchBar = () => {
                 manufacturer = {manufacturer}
                 setManufacturer = {setManufacturer}
             />
+            <SearchButton otherClasses="sm:hidden" />
         </div>
     </form>
   )
