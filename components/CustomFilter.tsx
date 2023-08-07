a0389ce4a4a922f60e46f7de7f1f2f24ea4831f5
valensixx <valensixx@gmail.com>
2023-08-07 14:22:15 +0300

    CustomFilter & SearchBar errors fixed

--- components/CustomFilter.tsx
+++ components/CustomFilter.tsx
@@ -3,12 +3,12 @@
 import React from 'react';
 import { Fragment, useState } from 'react';
 import Image from 'next/image';
-import { useRouter } from 'next/router';
+import { useRouter } from 'next/navigation';
 import { Listbox, Transition } from '@headlessui/react';
 import { CustomFilterProps } from '@/types';
 import { updateSearchParams } from '@/utils';
 
-const CustomFilter = ({ title, options }: CustomFilterProps) => {
+export default function CustomFilter({ title, options }: CustomFilterProps) {
   const router = useRouter();
   const [selected, setSelected] = useState(options[0]);
 
@@ -68,6 +68,5 @@ const CustomFilter = ({ title, options }: CustomFilterProps) => {
       </Listbox>
     </div>
   );
-};
+}
 
-export default CustomFilter;
--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -2,7 +2,7 @@
 
 import Image from "next/image";
 import React, { useState } from "react";
-import { useRouter } from "next/router";
+import { useRouter } from "next/navigation";
 
 import SearchManufacturer from "./SearchManufacturer";
 
